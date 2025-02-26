8f1e6508f609ef03e649720738791813d95f5084
Em Esh Thapa Magar <144942695+Em-Esh-Magar@users.noreply.github.com>
2025-02-26 10:25:28 +0545

    Day1 completed

---
+++ Day1/InputAndType.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number : "));
+b = int(input("Enter a divisible number : "));
+
+c = input("Enter any thing : ");
+
+print("The remainder is : ",a%b);
+print("The type of input is : ",type(c));
---
+++ Day1/SquareAndAverage.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a number : "));
+
+print("The square is : ",a*a);
+
+
+b = int(input("Enter a number : "));
+print("The average is : ",(a+b)/2);
+ 
